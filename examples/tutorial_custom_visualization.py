a665467d3abcf37e9c5271323c4af0d06df11789
Sven Wanner <s.wanner@dkfz-heidelberg.de>
2019-03-20 16:25:36 +0100

    update custom visualization tutorial

--- examples/tutorial_custom_visualization.py
+++ examples/tutorial_custom_visualization.py
@@ -11,23 +11,21 @@ from hyppopy.solver.RandomsearchSolver import RandomsearchSolver
 project = HyppopyProject()
 project.add_hyperparameter(name="axis_00", domain="uniform", data=[0, 1], dtype="float")
 project.add_hyperparameter(name="axis_01", domain="uniform", data=[0, 800], dtype="float")
-project.add_hyperparameter(name="axis_02", domain="uniform", data=[-1, 1], dtype="float")
-project.add_hyperparameter(name="axis_03", domain="uniform", data=[0, 5], dtype="float")
-project.add_hyperparameter(name="axis_04", domain="uniform", data=[1, 10000], dtype="float")
-project.add_hyperparameter(name="axis_05", domain="uniform", data=[0, 10], dtype="float")
+project.add_hyperparameter(name="axis_02", domain="uniform", data=[0, 5], dtype="float")
+project.add_hyperparameter(name="axis_03", domain="uniform", data=[1, 10000], dtype="float")
+project.add_hyperparameter(name="axis_04", domain="uniform", data=[0, 10], dtype="float")
 project.add_settings(section="solver", name="max_iterations", value=500)
 project.add_settings(section="custom", name="use_solver", value="hyperopt")
 
 plt.ion()
-fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8))
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(12, 8), sharex=True)
 plot_data = {"iterations": [],
              "loss": [],
              "axis_00": [],
              "axis_01": [],
              "axis_02": [],
              "axis_03": [],
-             "axis_04": [],
-             "axis_05": []}
+             "axis_04": []}
 
 
 def my_visualization_function(**kwargs):
@@ -39,21 +37,17 @@ def my_visualization_function(**kwargs):
     plot_data["axis_02"].append(kwargs['axis_02'])
     plot_data["axis_03"].append(kwargs['axis_03'])
     plot_data["axis_04"].append(kwargs['axis_04'])
-    plot_data["axis_05"].append(kwargs['axis_05'])
 
     axes[0, 0].clear()
     axes[0, 0].plot(plot_data["loss"], plot_data["axis_00"], ".r")
-    axes[0, 0].set_xlabel("loss")
     axes[0, 0].set_ylabel("axis_00")
 
     axes[0, 1].clear()
     axes[0, 1].plot(plot_data["loss"], plot_data["axis_01"], ".r")
-    axes[0, 1].set_xlabel("loss")
     axes[0, 1].set_ylabel("axis_01")
 
     axes[0, 2].clear()
     axes[0, 2].plot(plot_data["loss"], plot_data["axis_02"], ".r")
-    axes[0, 2].set_xlabel("loss")
     axes[0, 2].set_ylabel("axis_02")
 
     axes[1, 0].clear()
@@ -67,18 +61,18 @@ def my_visualization_function(**kwargs):
     axes[1, 1].set_ylabel("axis_04")
 
     axes[1, 2].clear()
-    axes[1, 2].plot(plot_data["loss"], plot_data["axis_05"], ".r")
+    axes[1, 2].plot(plot_data["loss"], plot_data["iterations"], ".r")
     axes[1, 2].set_xlabel("loss")
-    axes[1, 2].set_ylabel("axis_05")
+    axes[1, 2].set_ylabel("iterations")
 
     plt.draw()
     plt.tight_layout()
     plt.pause(0.001)
-    
+
 
 def my_loss_function(data, params):
     vfunc = VirtualFunction()
-    vfunc.load_default(6)
+    vfunc.load_default(5)
     return vfunc(**params)
 
 
