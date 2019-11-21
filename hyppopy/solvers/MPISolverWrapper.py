87002effbb4c3fa03bff08dd3dbed1b8bf9f3677
Andre Klein <andre.klein@dkfz-heidelberg.de>
2019-11-21 13:20:50 +0000

    Cleanup of MPISolverWrapper. Removed some TODOs and comments. Worker now also takes care of the

--- hyppopy/solvers/MPISolverWrapper.py
+++ hyppopy/solvers/MPISolverWrapper.py
@@ -9,6 +9,7 @@
 # A PARTICULAR PURPOSE.
 #
 # See LICENSE
+import datetime
 import os
 import logging
 
@@ -21,7 +22,7 @@ LOG.setLevel(DEBUGLEVEL)
 
 class MPISolverWrapper:
     """
-    TODO
+    TODO Class description
     The MPISolverWrapper class wraps the functionality of solvers in Hyppopy to extend them with MPI functionality.
     It builds upon the interface defined by the HyppopySolver class.
     """
@@ -57,27 +58,11 @@ class MPISolverWrapper:
         Just call get_results of the member solver and return the result.
         :return: return value of self._solver.get_results()
         """
-        # TODO: This is ugly.
+        # Only rank==0 returns results, the workers return None.
         mpi_rank = MPI.COMM_WORLD.Get_rank()
         if mpi_rank == 0:
             return self._solver.get_results()
         return None, None
-
-    def define_interface(self):
-        """
-        This function is called when HyppopySolver.__init__ function finished. Child classes need to define their
-        individual parameter here by calling the _add_member function for each class member variable need to be defined.
-        Using _add_hyperparameter_signature the structure of a hyperparameter the solver expects must be defined.
-        Both, members and hyperparameter signatures are later get checked, before executing the solver, ensuring
-        settings passed fullfill solver needs.
-        """
-        self._solver.define_interface()
-
-    def loss_function_call(self, params):
-        """
-        TODO: Do we even need this here? Don't think so.
-        """
-        return self._solver.loss_function_call(params)
 
     def run_worker_mode(self):
         """
@@ -93,21 +78,27 @@ class MPISolverWrapper:
         rank = comm.Get_rank()
         print("Starting worker {}. Waiting for param...".format(rank))
 
+        cand_results = dict()
         while True:
             candidate = comm.recv(source=0, tag=MPI_TAGS.MPI_SEND_CANDIDATE.value)  # Wait here till params are received
 
-            if candidate == None:
-                print(print("process {} received finish signal.".format(rank)))
+            if candidate is None:
+                print("[RECEIVE] Process {} received finish signal.".format(rank))
                 return
 
-            id = candidate.ID
-            params = candidate._definingValues
+            # if candidate.ID == 9999:
+            #     comm.gather(losses, root=0)
+            #     continue
 
+            cand_id = candidate.ID
+            params = candidate.get_values()
+
+            cand_results['book_time'] = datetime.datetime.now()
             loss = self._solver.blackbox.blackbox_func(None, params)
-            # RALF: Ergebnisse müssen zurück geschickt werden und nicht local in einem trial gespeichert werden
-            # TODO
-            # print('ID: {}, param: {}'.format(id, params))
-            comm.send((id,loss), dest=0, tag=MPI_TAGS.MPI_SEND_RESULTS.value)
+            cand_results['loss'] = loss  # Write loss to dictionary. This dictionary will be send back to the master via gather
+            cand_results['refresh_time'] = datetime.datetime.now()
+
+            comm.send((cand_id, cand_results), dest=0, tag=MPI_TAGS.MPI_SEND_RESULTS.value)
 
     @staticmethod
     def signal_worker_finished():
@@ -116,15 +107,14 @@ class MPISolverWrapper:
 
         :return:
         """
-        print('signal_worker_finished')
+        print('[SEND] signal_worker_finished')
         size = MPI.COMM_WORLD.Get_size()
         for i in range(size - 1):
             MPI.COMM_WORLD.send(None, dest=i + 1, tag=MPI_TAGS.MPI_SEND_CANDIDATE.value)
 
     def run(self, *args, **kwargs):
         """
-        This function starts the optimization process.
-        :param print_stats: [bool] en- or disable console output
+        This function starts the optimization process of the underlying solver and takes care of the MPI awareness.
         """
 
         mpi_rank = MPI.COMM_WORLD.Get_rank()
@@ -136,28 +126,18 @@ class MPISolverWrapper:
             # this script execution should be in worker mode as it is an mpi worker.
             self.run_worker_mode()
 
-    def convert_searchspace(self, hyperparameter):
-        """
-        Just a call of the convert_searchspace function of the member solver.
+    @staticmethod
+    def is_master():
+        mpi_rank = MPI.COMM_WORLD.Get_rank()
+        if mpi_rank == 0:
+            return True
+        else:
+            return False
 
-        The function converts the standard parameter input into a range list depending
-        on the domain. These rangelists are later used with itertools product to create
-        a paramater space sample of each combination.
-
-        :param hyperparameter: [dict] hyperparameter space
-
-        :return: [list] name and range for each parameter space axis
-        """
-
-        searchspace = self._solver.convert_searchspace(hyperparameter)
-
-        return searchspace
-
-    def loss_function(self, params):
-        return self._solver.loss_function(**params)
-
-    def print_best(self):
-        self._solver.print_best()
-
-    def print_timestats(self):
-        self._solver.print_timestats()
+    @staticmethod
+    def is_worker(self):
+        mpi_rank = MPI.COMM_WORLD.Get_rank()
+        if mpi_rank != 0:
+            return True
+        else:
+            return False
