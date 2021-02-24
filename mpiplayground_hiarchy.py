d6ec11f65d514dd51d4f0d54f40b89a0aafbdb4b
Ralf Floca <r.floca@dkfz-heidelberg.de>
2021-02-24 12:19:18 +0100

    Fleshed out the draft a bit more.

--- mpiplayground_hiarchy.py
+++ mpiplayground_hiarchy.py
@@ -45,13 +45,31 @@ project = HyppopyProject(config=config)
 # downstream_comm can be used to communicate with the workers on tier 1
 # associated with the respective tier 0 worker, their tier head node.
 def my_loss_function(x, y, downstream_comm):
-    return x**2+y**3
+    # here one can communicate downstream to distribute work into tier 1
+    # or receive results from tier 1
+    size = downstream_comm.Get_size()
+
+    #distribute work downstream
+    for i in range(size - 1):
+        downstream_comm.send({'x':x,'y':y}, dest=i+1)
+
+    # do own work
+
+    #collect stuff from worker
+    z = 0
+    for i in range(size - 1):
+        z += MPI.downstream_comm.recv(source=i + 1)
+
+    # finally the loss function has to return the computed loss
+    return z
 
 def my_cascade_callback(tier_id, upstream_comm, downstream_comm, blackbox):
     """Example callback that is used for the worker code on subsequent tiers.
        This function is called by MPICascadedSolverWrapper for the respective
        tier.
        If this function returns, it is assumed that the node has finished.
+       For tier 0 the function only is called after all evaluations have been
+       made and the optimization process has finished.
        :param tier_id: index of the tier the node is as a worker. As tier 0
         is handled by the MPIBlackboxFunction, this callback is
         used for all subsequent tiers.
@@ -66,15 +84,37 @@ def my_cascade_callback(tier_id, upstream_comm, downstream_comm, blackbox):
        :param blackbox: the instance of the blackbox function (not in its MPI
        wrapper).
     """
-    #here you can manage the worker code.
-    #upstream_comm is the communicator used to talk with the primary of the current node.on the sdd
+    # here one can manage the worker code.
+    # Depending on the tier id you can implement different communication patterns
+    # with upstream or downstream as well as the workload code for the node.
+    # In this example we only have 2 tiers so only tier 1 will be handled
+    # here and we don't need tier specific logics
+    if tier_id == 0:
+        # worker on tier 0 only reach this point if all evaluations have been done
+        # end it is just about cleaning up and releasing all nodes.
+        size = downstream_comm.Get_size()
+        for i in range(size - 1):
+            downstream_comm.send(None, dest=i + 1)
+    else:
+        # Here we are on tier 1 and do the additional computations for the upstream
+        while True:
+            params = upstream_comm.recv(source=0)
 
-solver = MPICascadedSolverWrapper(solver=SolverPool.get(project=project), layout = [4,10,5], cascade_callback = my_cascade_callback)
+            if params is None:
+                #nothing to do any more
+                return
+
+            rank = upstream_comm.Get_rank()
+            z = rank + params['x']**2 + params['y']**3
+            upstream_comm.send(z, dest=0)
+
+
+solver = MPICascadedSolverWrapper(solver=SolverPool.get(project=project), blackbox = my_loss_function, layout = [4,10,5], cascade_callback = my_cascade_callback)
+
+#alternative call/init pattern
+solver = MPICascadedSolverWrapper(solver=SolverPool.get(project=project))
 solver.blackbox = my_loss_function
-
-#alternative call
-solver = MPICascadedSolverWrapper(solver=SolverPool.get(project=project), layout = [4,10,5])
-solver.layout = [3,4,5]
+solver.layout = [3,5]
 solver.cascade_callback = my_cascade_callback
 # maybe also
 # solver.cascade_callbacks[2] = dedicated my_tier_calback for the 3rd tier
